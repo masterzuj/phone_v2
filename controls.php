a1ca00d1fd9d5727aa5b07a2427a7882df56c6d8
Robert Zujkov <robert.zujkov@gmail.com>
2025-05-16 03:02:36 +0200

    session functions

---
+++ controls.php
@@ -0,0 +1,57 @@
+<?php 
+session_start();
+
+if(isset($_POST["name_user"])){
+	$_SESSION["name_user"] = trim(htmlspecialchars($_POST["name_user"]));
+}
+
+//echo $_SESSION["name_user"] ;
+?>
+
+<meta charset="UTF-8">
+
+<script src="./jquery-3.7.1.js" sourcemappingurl="./jquery-3.7.1.min.map"></script>
+
+<style>
+
+#menu{
+	position: absolute; 
+	background-color: lightgray;
+	height: 40px;
+	width: 450px;
+	border-radius: 10px;
+}
+#menubild{
+	height: 40px;
+	width: 40px;
+}
+
+</style>
+
+<div id="menu">
+	<div style="position: absolute; left: 10px; font-size: 30px;" id="user_container">
+		<form action="./controls.php" method="POST">
+			<input type="submit" style="width: 30px;" value="ok" id="user_ok"/>
+			<input type="text" name="name_user" id="user_name" style="width: 130px;" value="<?php echo $_SESSION['name_user']; ?>" placeholder="user name"/>
+		</form>
+	</div>
+	
+	<div style="position: absolute; left: 200px; font-size: 30px;"><a onclick="window.parent.location.href = 'index.php';">⌂</a></div>
+	<div style="position: absolute; left: 250px; color: black; font-size: 20px; margin-top: 10px;">ⓩⓤⓚⓤⓝⓕⓣ</div>
+	<div style="position: absolute; right: 0px; font-size: 30px;"><a onclick="history.back()">🔙</a></div>
+	
+</div>
+<script>
+$(document).ready(function() {
+	$(window).click(function() {
+	  $("#user_ok").hide("fast");
+		$("#user_container").css("top", "10px");
+	});
+
+	$("#user_name").on("click", function(event) {
+		$("#user_ok").show("fast");
+		$("#user_container").css("top", "0px");
+		event.stopPropagation();
+	});
+});
+</script>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+<meta charset="UTF-8">
+
+<title>zu phone</title>
+
+<script src="./jquery-3.7.1.js" sourcemappingurl="./jquery-3.7.1.min.map"></script>
+ 
+<script src="./jquery-ui-1.14.1/jquery-ui-1.14.1/jquery-ui.js"></script>
+  
+<link rel="stylesheet" href="./jquery-ui-1.14.1/jquery-ui-1.14.1/jquery-ui.css">
+
+
+<script>
+
+console.log("███████╗██╗   ██╗██╗  ██╗██╗   ██╗███╗   ██╗███████╗████████╗");
+console.log("╚══███╔╝██║   ██║██║ ██╔╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝");
+console.log("  ███╔╝ ██║   ██║█████╔╝ ██║   ██║██╔██╗ ██║█████╗     ██║   ");
+console.log(" ███╔╝  ██║   ██║██╔═██╗ ██║   ██║██║╚██╗██║██╔══╝     ██║   ");
+console.log("███████╗╚██████╔╝██║  ██╗╚██████╔╝██║ ╚████║██║        ██║   ");
+console.log("╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝        ╚═╝   ");
+                                                             	   
+      																	  
+</script>
+
+
+<div style="float: left;">
+<iframe src="./menu.html" style="width: 450px; height: 750px;" scrolling="no" frameBorder="0"></iframe><br/>
+<iframe src="./controls.php" style="width: 460px; height: 50px;" scrolling="no" frameBorder="0"></iframe>
+</div>
+<div style="float: left;">
+<iframe style="width: 500px; height: 750px; " src="https://zujkov.com/phone/phone.html" frameBorder="0" scrolling="no"></iframe>
+<!--
+<iframe style="width: 500px; height: 750px; " src="https://zujkov.com/hand/hand.php" frameBorder="0" scrolling="no"></iframe>
+<iframe style="width: 500px; height: 750px; " src="https://zujkov.com/hand_three/hands11/index.html" frameBorder="0" scrolling="no"></iframe>
+-->
+</div>
+
+
+
+<div style="position:fixed; bottom: 0px; right: 0px; font-size: 17px; background-color: white;"><a href="https://itrk.legal/QqA.8V.KaB.html?impressum-datenschutz" target="_blank" style="font-size: 17px; color: black;">Impressum | Datenschutzerklärung</a><br/>(c) ZUJKOV COM | all rights reserved</div>
